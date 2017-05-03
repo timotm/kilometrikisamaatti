e0bddd876c49f100fb108ff51d8b4a9c2b068698
Timo Metsälä <timo.metsala@reaktor.fi>
2017-05-03 07:35:35 +0300

    update contest id for 2017k

--- kmapi.js
+++ kmapi.js
@@ -68,7 +68,7 @@ function doKmKisaPostKmForDate(kk_login, kk_password, datestr, kms) {
                             uri: 'https://www.kilometrikisa.fi/contest/log-save/',
                             form: { csrfmiddlewaretoken: csrftoken,
                                     km_amount: kms.toString().replace('.', ','),
-                                    contest_id: "21",
+                                    contest_id: "22",
                                     km_date: datestr },
                           })
     })
